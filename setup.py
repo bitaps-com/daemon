a158dc1dad3f4c3d84b9bb04f369a7b96cf8064d
Aleksey Karpov <admin@bitaps.com>
2016-05-21 11:26:25 -0400

    setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='daemon',
+      version='1.0',
+      description='Python daemonizer package',
+      author='Alexsei Karpov',
+      author_email='karpov.alexsei@bitaps.com',
+      url='http://git.bitaps.com/4tochka/daemon.git',
+      packages=['daemon', ],
+     )
