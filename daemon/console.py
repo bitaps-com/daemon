2df5906e9bac6d28846ce752af82395b403d7da8
Aleksey Karpov <admin@bitaps.com>
2017-04-17 01:35:44 -0400

    fix

--- daemon/console.py
+++ daemon/console.py
@@ -233,7 +233,6 @@ class Console:
         if cmd == 'kill':
             PID_FILE = getattr(sys.modules['__main__'], 'PID_FILE')
             f = open(PID_FILE, 'r')
-            print("PID_FILE %s" % PID_FILE)
             i = f.read()
             f.close()
             try:
